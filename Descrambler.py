5ac95096647f5a7ab7feb45f8f371d83a2f7b09d
ssplat312 <cayden.rivers@gmail.com>
2025-01-13 21:10:45 +0000

    Made descrambler better and changed api used.

--- Descrambler.py
+++ Descrambler.py
@@ -1,10 +1,26 @@
 import requests
+import time
 
 def factorial(num):
     newNum = 1
     for i in range(2, num + 1):
         newNum = newNum * i
     return newNum
+
+def GetConfidence(word, desiredWord):
+    
+    confidence = (len(word)/len(desiredWord)) * 100 if len(word) <= len(desiredWord) else (len(desiredWord)/len(word)) * 100
+    confidence = confidence/2
+    charInSet = 0
+    charsUsed = set(word)
+
+    for letter in desiredWord:
+        if letter in charsUsed:
+            charInSet = charInSet + 1
+
+    confidence = confidence + ((charInSet/len(desiredWord) * 100)/2)
+
+    return confidence
 
 def GetPossibleWords(baseWord, curLength):    
     startPos = len(baseWord) - curLength
@@ -32,25 +48,57 @@ def GetWordCombinations(baseWord, curLength):
         else:
             print(baseWord)
 
-def CheckWord(name):
-    url = f"{baseUrl}{name}"
+def CheckWord(word):
+    url = "https://mashape-community-urban-dictionary.p.rapidapi.com/define"
 
-    response = requests.get(url)
+    querystring = {"term":word}
 
-    if(response.status_code == 200):
-        data = response.json()
-        print(f"{data[0]["word"]}")
+    headers = {
+        "x-rapidapi-key": "a85dcfd916msh4297cecd3f4bb06p16e23cjsn05408e18591a",
+        "x-rapidapi-host": "mashape-community-urban-dictionary.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers, params=querystring)
+
+    if response.status_code == 200:
+        PrintWord(response.json())
+
+def PrintWord(wordsInfo):
+    answer = "y"
+    curWord = 0
+    maxWord = len(wordsInfo["list"])
     
+    for wordInfo in wordsInfo["list"]:
+        cofindence = GetConfidence(wordInfo["word"], name)
+        if desiredConfidence > cofindence:
+            print(f"Word only had {cofindence} instead of {desiredConfidence} or above")
+            continue
 
-baseUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/"
+        if answer != 'y' and answer != "Y":
+            break
+        curWord = curWord + 1
+        print(f"1.{wordInfo["word"]}: {wordInfo["definition"]}")
+        print(f"Example sentence: {wordInfo["example"]}\n")
+        time.sleep(3)
+        if curWord < maxWord:
+            print(f"You are on word {curWord} of {maxWord}.\n Press Y/y to see the next one, anything else to stop.")
+            answer = input("Input here:")
+
+
 name = input("Input a word you want to descramble: ")
 answer = input("Do you want to descramble this word or get the possible combinations(Y/y for descramble and N/n for combinations): ")
+
+
 print(f"Their are {factorial(len(name))} possible combinations of the word {name}.")
+
 if answer == 'y' or answer == 'Y':
+    #confidence is the length of the word found over the length of letters in the scrambled word
+    desiredConfidence = int(input("What confidence do you want to have for the unscrambled words(choose between inclusive 1 and 100)?: "))
     print(f"The possible words that can be made from {name} are:")
     GetPossibleWords(name, len(name))
 else:
     print(f"The combinations of words that can be made from {name} are:")
     GetWordCombinations(name, len(name))
     print(f"Here are {factorial(len(name))} combinations of the word {name}.")
+
 print("Finished")
